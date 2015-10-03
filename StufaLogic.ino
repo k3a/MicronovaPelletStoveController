dd5e71a33858b290a4ba039087fcb4cecdaf719f
Mario <mario@ndemiccreations.com>
2015-10-03 19:26:14 +0200

    added arduino sketch for NC7WZ07 logic emulation

---
+++ StufaLogic.ino
@@ -0,0 +1,42 @@
+const int LED_pin = 13;
+const int IN_pin = 2;
+const int OUT_pin = 3;
+
+void in_rise()
+{
+  pinMode(OUT_pin, INPUT);
+  
+  digitalWrite(LED_pin, LOW);
+}
+
+void in_fall()
+{
+  pinMode(OUT_pin, OUTPUT);
+  digitalWrite(OUT_pin, LOW);
+  
+  digitalWrite(LED_pin, HIGH);
+}
+
+void in_change()
+{
+  if (digitalRead(IN_pin) == HIGH)
+    in_rise();
+  else
+    in_fall();
+}
+
+void setup() {
+  pinMode(LED_pin, OUTPUT);
+  
+  pinMode(IN_pin, INPUT);
+  attachInterrupt(digitalPinToInterrupt(IN_pin), in_change, CHANGE);
+  
+  pinMode(OUT_pin, INPUT);
+}
+
+void loop() {
+/*  digitalWrite(LED_pin, HIGH);
+  delay(2000);
+  digitalWrite(LED_pin, LOW);
+  delay(2000);*/
+}
